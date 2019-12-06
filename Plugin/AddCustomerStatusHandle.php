8ed4a98fb20895e87fb141a596a467567cf13447
Siarhey <siarhey.uchukhlebau@gmail.com>
2019-12-06 13:37:58 +0300

    Add new handles for category_view_page

--- Plugin/AddCustomerStatusHandle.php
+++ Plugin/AddCustomerStatusHandle.php
@@ -33,12 +33,34 @@ class AddCustomerStatusHandle
     public function afterAddDefaultHandle(
         \Magento\Framework\View\Result\Layout $subject,
         \Magento\Framework\View\Result\Layout $result
-    )
-    {
+    ) {
         if ($this->customerSession->getCustomerId()) {
             $result->addHandle('customer_logged_in');
         } else {
             $result->addHandle('customer_logged_out');
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param \Magento\Framework\View\Result\Layout $subject
+     * @param \Magento\Framework\View\Result\Layout $result
+     */
+    public function afterAddHandle(
+        \Magento\Framework\View\Result\Layout $subject,
+        \Magento\Framework\View\Result\Layout $result,
+        $handleName
+    ) {
+        if ($handleName == 'catalog_category_view') {
+            $availableHandles = $result->getLayout()->getUpdate()->getHandles();
+            if ($this->customerSession->getCustomerId() &&
+                !in_array('catalog_category_view_customer_logged_in', $availableHandles)
+            ) {
+                $result->addHandle('catalog_category_view_customer_logged_in');
+            } elseif (!in_array('catalog_category_view_customer_logged_out', $availableHandles)) {
+                $result->addHandle('catalog_category_view_customer_logged_out');
+            }
         }
 
         return $result;
